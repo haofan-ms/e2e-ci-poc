6fa030c00460f44717303ada178284728dc6fe0b
unknown <Administrator@n42r0702-DVM.n42r0702.masd.stbtest.microsoft.com>
2020-04-22 03:03:59 +0000

    update

--- scripts/setup-dvm.sh
+++ scripts/setup-dvm.sh
@@ -5,6 +5,10 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+if [ -d "/e2e-ci-poc" ]
+then
+    sudo rm -rf /e2e-ci-poc
+fi
 git clone https://github.com/haofan-ms/e2e-ci-poc -b test
 
 REPO_ROOT=e2e-ci-poc
